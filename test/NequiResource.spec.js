daad49a2bd87b27c29724fbdb8d27d3e384c0465
Juan P Lopez <dolcalmi@gmail.com>
2018-01-06 15:47:21 -0500

    add resource tests

---
+++ test/NequiResource.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('./testUtils');
+
+var nequi = require('./testUtils').getSpyableNequi();
+var expect = require('chai').expect;
+
+describe('NequiResource', function() {
+  describe('createResourcePathWithSymbols', function() {
+    it('Generates a path', function() {
+      nequi.pushPayments.create({});
+      var path = nequi.pushPayments.createResourcePathWithSymbols('{id}');
+      expect(path).to.equal('/{id}');
+    });
+  });
+
+  describe('_defaultHeaders', function() {
+    it('sets the api key header using the global API key', function() {
+      var headers = nequi.pushPayments._defaultHeaders(null, 0, null);
+      expect(headers['x-api-key']).to.equal('fakeApiKey');
+    });
+    it('sets the api key header using the specified API key', function() {
+      var headers = nequi.pushPayments._defaultHeaders('anotherFakeApiKey', 0, null);
+      expect(headers['x-api-key']).to.equal('anotherFakeApiKey');
+    });
+  });
+});
--- test/nequi.spec.js
+++ test/nequi.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Promise = require('bluebird');
 var testUtils = require('./testUtils');
 var nequi = require('../lib/nequi')(
   testUtils.getUserNequiAccessKey(),
@@ -8,13 +9,18 @@ var nequi = require('../lib/nequi')(
 );
 
 var DEFAULT_CLIENT_ID = 'nequi-node';
+var PAYMENT_DETAILS = {
+  phoneNumber: '3009871234',
+  code: '1',
+  value: '100'
+};
 
 var expect = require('chai').expect;
 
 describe('Nequi Module', function() {
   this.timeout(20000);
 
-  describe('setClientId', function() {
+  describe('Set client id', function() {
     it('Should define a default equal to nequi-node', function() {
       expect(nequi.getApiField('clientId')).to.equal(DEFAULT_CLIENT_ID);
     });
@@ -30,7 +36,7 @@ describe('Nequi Module', function() {
     });
   });
 
-  describe('setTimeout', function() {
+  describe('Set timeout', function() {
     it('Should define a default equal to the node default', function() {
       expect(nequi.getApiField('timeout')).to.equal(require('http').createServer().timeout);
     });
@@ -43,4 +49,30 @@ describe('Nequi Module', function() {
       expect(nequi.getApiField('timeout')).to.equal(require('http').createServer().timeout);
     });
   });
+
+  describe('Callback support', function() {
+      describe('Any given endpoint', function() {
+        it('Will call a callback if successful', function() {
+          return expect(new Promise(function(resolve, reject) {
+            nequi.pushPayments.create(PAYMENT_DETAILS, function(err, payment) {
+              resolve('Called!');
+            });
+          })).to.eventually.equal('Called!');
+        });
+
+        it('Given an error the callback will receive it', function() {
+          return expect(new Promise(function(resolve, reject) {
+            nequi.pushPayments.create(PAYMENT_DETAILS, function(err, payment) {
+              if (err) {
+                resolve('ErrorWasPassed');
+              } else {
+                reject(new Error('NoErrorPassed'));
+              }
+            });
+          })).to.eventually.become('ErrorWasPassed');
+        });
+      });
+    });
+
+
 });
